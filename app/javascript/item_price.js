e00d55c0d302b1a59e0d3a0a1290cf918c5294d9
makoiwa <whitepaper.bass@gmail.com>
2024-01-13 21:29:18 +0900

    issue6

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,8 +1,10 @@
-let priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
-  const inputValue = priceInput.value;
-  const addTaxDom = document.getElementById("add-tax-price");
-  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-  const salesProfit = document.getElementById("profit");
-  salesProfit.innerHTML = inputValue - Math.floor(inputValue * 0.1);
-})
+document.addEventListener("turbo:load", () => {
+  let priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    const salesProfit = document.getElementById("profit");
+    salesProfit.innerHTML = inputValue - Math.floor(inputValue * 0.1);
+  });
+});
