831ddd30fc5be1d60f45e3fa61f1426c3d0a2820
makoiwa <whitepaper.bass@gmail.com>
2024-01-18 20:59:21 +0900

    issue8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :check_product_user, only: [:edit, :update]
 
   def index
     @items = Item.order("created_at DESC").all
@@ -19,11 +20,31 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @items = Item.find(params[:id])
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:item, :explanation, :category_id, :condition_id, :delivery_cost_id, :prefectures_id, :shipment_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def check_product_user
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 end
